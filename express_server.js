473a9dcb9dec5c807fa5658d1f00664246977e7a
Darren Picard <darrenpicard25@gmail.com>
2019-02-12 22:24:15 +0000

    Making all the changes

--- express_server.js
+++ express_server.js
@@ -27,6 +27,14 @@ app.get("/urls" , (req, res) => {
   res.render("urls_index", templateData);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
