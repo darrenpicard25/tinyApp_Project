86b578a362d5453e6f928c6fdf0a57f08ea89540
Darren Picard <darrenpicard25@gmail.com>
2019-02-12 20:21:04 +0000

    Started server code

---
+++ express_server.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let PORT = 8080; //this is default port
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+app.get("/", (req, res) => {
+  res.send('hello');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
