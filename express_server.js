3348798309b85a14bbb376e2038d08197cbf4891
Darren Picard <darrenpicard25@gmail.com>
2019-02-13 22:20:11 +0000

    Finished edit feature to change URL's in the database

--- express_server.js
+++ express_server.js
@@ -14,36 +14,45 @@ function generateRandomString() {
   }
   return output;
 }
-
-
+//Our Data
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-
+//Root Page But not set up
 app.get("/", (req, res) => {
   res.send('hello');
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+//Main urls Page
 app.get("/urls" , (req, res) => {
   let templateData = { urls: urlDatabase};
   res.render("urls_index", templateData);
 });
 
+app.post("/urls/delete", (req, res) => {
+  delete urlDatabase[req.body.id];
+  res.redirect("/urls");
+});
+
+//Just looks at our database no link to page
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+//Page with form to add new URL
 app.get("/urls/new", (req, res) => {
   let templateData = { urls: urlDatabase};
   res.render("urls_new", templateData);
 });
+//Post to page from the form
+app.post("/urls", (req, res) => {
+    let shortURL = generateRandomString();
+    let longURL = req.body.longURL;
+    urlDatabase[shortURL] = longURL;
+    res.redirect(`urls/${shortURL}`);
+});
 
+//Look at details of each URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
@@ -52,16 +61,19 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post('/edit/:id' , (req, res)=> {
+  id =req.params.id;
+  console.log(req.body);
+  newURL =req.body.newURL;
+  urlDatabase[id] = newURL;
+  res.redirect('/urls');
+});
+
+//Redirect to actual Page
 app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 
-app.post("/urls", (req, res) => {
-    let shortURL = generateRandomString();
-    let longURL = req.body.longURL;
-    urlDatabase[shortURL] = longURL;
-    res.redirect(`urls/${shortURL}`);
-});
 
 
 app.listen(PORT, () => {
